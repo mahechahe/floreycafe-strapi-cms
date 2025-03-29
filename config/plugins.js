43c4cf949719d95cd70031e65ce850e7e2bcca5d
Estiven Mahecha <mahechahe@gmail.com>
2025-03-29 13:37:51 -0500

    update upload provider to use AWS S3 with configuration options

--- config/plugins.js
+++ config/plugins.js
@@ -6,7 +6,15 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: "local",
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET_NAME"),
+        },
+      },
       actionOptions: {
         upload: {},
         delete: {},
